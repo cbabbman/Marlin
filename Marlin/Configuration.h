7c8ce5f4c9b5044fda8f815c6e9915a07baf9d92
cbabbman <cbabbman@gmail.com>
2020-07-07 18:02:25 -0500

    Update Configuration.h

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -726,6 +726,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  * Bondtech BMG uses 415 steps on extruder instead of 93
+ */
 #define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 428.70 }
 
 /**
